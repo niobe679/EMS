2f279436a807fd7ce59f6f0899aa7b6fff56eac7
niobe679 <59116786+niobe679@users.noreply.github.com>
2024-09-30 22:14:49 +0300

    Update Header.js

--- src/Page/Dashboard/Header.js
+++ src/Page/Dashboard/Header.js
@@ -5,7 +5,7 @@ function Header({ setIsAdding }) {
         <header>
             <h1>Employee Management Software</h1>
             <div style={{ marginTop: '30px', marginBottom: '18px' }}>
-                <button onClick={() => setIsAdding(true)} className='round-button'>Add Button</button>
+                <button onClick={() => setIsAdding(true)} className='round-button'>Add Employee</button>
             </div>
         </header>
     )
